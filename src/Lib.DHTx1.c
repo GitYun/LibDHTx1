27112e90c6af8875ac9d58bcd52735200a1b61a6
GitYun <youneihai@126.com>
2020-11-01 19:52:39 +0800

    1. [Fixed]DHTx1驱动 2. [Add]STC12单片机的测试代码

--- src/Lib.DHTx1.c
+++ src/Lib.DHTx1.c
@@ -1,12 +1,12 @@
 /**
  * \file Lib.DHTx1.c
  * \author vEmagic (admin@vemgic.com)
- * \brief 
+ * \brief
  * \version 0.1.0
  * \date 2020-10-29
- * 
+ *
  * Copyright 2020 (C), Sichuan Deyang DeYi Electronic Instrument Co., Ltd.
- * 
+ *
  */
 
 #include <string.h>
@@ -35,20 +35,24 @@ int8_t DHTx1_Init(tDHTx1Sensor* psDHTx1)
 /* Open the DHTx1 Device */
 tDHTx1Status DHTx1_Open(tDHTx1Sensor* psDHTx1)
 {
+    (void)psDHTx1;
+
     /* Start condition: I/O pin output low level 20ms */
-    DHTx1_DATA_PIN_OUTPUT_MODE(GPIO_PIN(psDHTx1->psGPIO));
-    DHTx1_DATA_PIN_LOW(GPIO_PIN(psDHTx1->psGPIO));
+    DHTx1_DATA_PIN_OUTPUT_MODE(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
+    DHTx1_DATA_PIN_LOW(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
     DHTx1_DelayUs(20000);
 
-    DHTx1_DATA_PIN_HIGH(GPIO_PIN(psDHTx1->psGPIO));
+    DHTx1_DATA_PIN_HIGH(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
     DHTx1_DelayUs(20); // Pull-hgih level 20-40us
 
-    DHTx1_DATA_PIN_INPUT_MODE(GPIO_PIN(psDHTx1->psGPIO));
+    DHTx1_DATA_PIN_INPUT_MODE(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
 
-    if (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 0)
+    if (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 0)
     {
         uint8_t ui8Timeout = 0;
-        while (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 0 && ++ui8Timeout < 100)
+
+        // Skip DHTx1 response low level time about 80us
+        while (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 0 && ++ui8Timeout < 100)
         {
             DHTx1_DelayUs(1);
         }
@@ -58,8 +62,9 @@ tDHTx1Status DHTx1_Open(tDHTx1Sensor* psDHTx1)
             return eDHTx1_OPEN_RSP_LOW_ERROR;
         }
 
+        // Skip DHTx1 response high level time about 80us
         ui8Timeout = 0;
-        while (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 1 && ++ui8Timeout < 100)
+        while (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 1 && ++ui8Timeout < 100)
         {
             DHTx1_DelayUs(1);
         }
@@ -69,16 +74,20 @@ tDHTx1Status DHTx1_Open(tDHTx1Sensor* psDHTx1)
             return eDHTx1_OPEN_RSP_HIGH_ERROR;
         }
 
-        return eDHTx1_OPENED;
+        return eDHTx1_OPENED; // DHTx1 open sucessful: start condition correct
     }
+
+    return eDHTx1_INVAILD; // DHTx1 open failed
 }
 /*-----------------------------------------------------------------------------------------------*/
 
 /* Close the DHTx1 Device */
-bool DHTx1_Close(tDHTx1Sensor* psDHTx1) 
+bool DHTx1_Close(tDHTx1Sensor* psDHTx1)
 {
-    DHTx1_DATA_PIN_OUTPUT_MODE(GPIO_PIN(psDHTx1->psGPIO));
-    DHTx1_DATA_PIN_HIGH(GPIO_PIN(psDHTx1->psGPIO));
+    (void)psDHTx1;
+
+    DHTx1_DATA_PIN_OUTPUT_MODE(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
+    DHTx1_DATA_PIN_HIGH(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin);
 
     return true;
 }
@@ -89,14 +98,15 @@ uint8_t DHTx1_ReadByte(tDHTx1Sensor* psDHTx1)
 {
     uint8_t ui8Result = 0;
     uint8_t ui8Timeout = 0;
-
     uint8_t idx;
+    (void)psDHTx1;
+
     for (idx = 0; idx < 8; ++idx)
     {
         ui8Result <<= 1;
 
         // Wait 50us before read one-wire bus data
-        while (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 0 && ++ui8Timeout < 60);
+        while (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 0 && ++ui8Timeout < 60);
         {
             DHTx1_DelayUs(1);
         }
@@ -109,14 +119,14 @@ uint8_t DHTx1_ReadByte(tDHTx1Sensor* psDHTx1)
         // Delay 30us, aim to jump data '0'
         DHTx1_DelayUs(30);
 
-        if (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 1) // Read data '1'
+        if (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 1) // Read data '1'
         {
             ui8Result |= 0x01;
         }
-        
+
         // Wait data '1' finish
         ui8Timeout = 0;
-        while (DHTx1_DATA_PIN_READ(GPIO_PIN(psDHTx1->psGPIO)) == 1 && ++ui8Timeout < 60)
+        while (DHTx1_DATA_PIN_READ(psDHTx1->psGPIO->sPort, psDHTx1->psGPIO->sPin) == 1 && ++ui8Timeout < 60)
         {
             DHTx1_DelayUs(1);
         }
@@ -136,18 +146,18 @@ bool DHTx1_ReadData(tDHTx1Sensor* psDHTx1)
 {
     bool bRet = false;
 
-    if (psDHTx1->sStatus == eDHTx1_OPENED)
+    if (DHTx1_Open(psDHTx1) == eDHTx1_OPENED)
     {
+        uint8_t idx;
         memset(psDHTx1->pui8Data, 0, sizeof(psDHTx1->pui8Data));
 
-        uint8_t idx;
         for (idx = 0; idx < 5; ++idx)
         {
             psDHTx1->pui8Data[idx] = DHTx1_ReadByte(psDHTx1);
         }
 
         bRet = psDHTx1->pui8Data[4] == (uint8_t)(psDHTx1->pui8Data[0] + psDHTx1->pui8Data[1]
-            +psDHTx1->pui8Data[2] + psDHTx1->pui8Data[2]);
+            +psDHTx1->pui8Data[2] + psDHTx1->pui8Data[3]);
     }
 
     return bRet;
@@ -173,15 +183,17 @@ bool DHTx1_IsShortOfReadTime(tDHTx1Sensor* psDHTx1, uint32_t ui32IntervalMilliSe
 /*-----------------------------------------------------------------------------------------------*/
 
 /* Check the data whether has been changed */
-bool DHTx1_IsDataChanged(tDHTx1Sensor* psDHTx1, uint16_t ui16PreviousData)
+bool DHTx1_IsDataChanged(tDHTx1Sensor* psDHTx1, uint8_t* pui8PreviousData)
 {
+    (void)psDHTx1;
+    (void)pui8PreviousData;
     return true;
 }
 /*-----------------------------------------------------------------------------------------------*/
 
 /* Get the temperature value, the value scale 10 time */
 uint16_t DHTx1_GetTemperature(tDHTx1Sensor* psDHTx1)
-{    
+{
     uint16_t ui16Temperature = 0;
 
     if (psDHTx1->sId == eDHT11)
@@ -207,25 +219,45 @@ uint16_t DHTx1_GetHumidity(tDHTx1Sensor* psDHTx1)
         ui16Humidity = (uint16_t)psDHTx1->pui8Data[0] * 10 + psDHTx1->pui8Data[1];
     }
     else if (psDHTx1->sId == eDHT21)
-    [
+    {
         ui16Humidity = ((uint16_t)psDHTx1->pui8Data[0] << 8) | psDHTx1->pui8Data[1];
-    ]
+    }
 
     return ui16Humidity;
 }
 /*-----------------------------------------------------------------------------------------------*/
 
+/* Get the humidity and temperature value, all value scale 10 time */
+void DHTx1_GetHumidityAndTemperature(tDHTx1Sensor* psDHTx1,
+    uint16_t* pui16Humidity, uint16_t* pui16Temperature)
+{
+    if (psDHTx1->sId == eDHT11)
+    {
+        *pui16Humidity = (uint16_t)psDHTx1->pui8Data[0] * 10 + psDHTx1->pui8Data[1];
+        *pui16Temperature = (uint16_t)psDHTx1->pui8Data[2] * 10 + psDHTx1->pui8Data[3];
+    }
+    else if (psDHTx1->sId == eDHT21)
+    {
+        *pui16Humidity = ((uint16_t)psDHTx1->pui8Data[0] << 8) | psDHTx1->pui8Data[1];
+        *pui16Temperature = ((uint16_t)psDHTx1->pui8Data[2] << 8) | psDHTx1->pui8Data[3];
+    }
+}
+/*-----------------------------------------------------------------------------------------------*/
+
+#if defined (STM32) || defined (__CC_ARM)  || defined (__GNUC__)
 /* Convert the temperature from celsius to fahrenheit */
 float DHTx1_ConvertC2F(uint16_t ui16Celsius)
 {
-    return (float)1.8 * ui16Celsius / 10.0 + 32.0;
+    // fahrenheit = 1.8 * celsius + 32.0
+    return ((uint32_t)18 * ui16Celsius + 3200UL) / 100.0f;
 }
 /*-----------------------------------------------------------------------------------------------*/
 
 /* Convert the temperature from celsius to kai */
 float DHTx1_ConvertC2K(uint16_t ui16Celsius)
 {
-    return (float)ui16Celsius / 10.0 + 273.15;
+    // kai = celesius + 273.15
+    return (ui16Celsius + 2731) / 10.0f;
 }
 /*-----------------------------------------------------------------------------------------------*/
 
@@ -233,10 +265,13 @@ float DHTx1_ConvertC2K(uint16_t ui16Celsius)
 // 参考: http://wahiduddin.net/calc/density_algorithms.htm
 float DHTx1_DewPoint(uint16_t ui16Celsius, uint16_t ui16Humidity)
 {
+    (void)ui16Celsius;
+    (void)ui16Humidity;
     return 0;
 }
 /*-----------------------------------------------------------------------------------------------*/
 
+#include <math.h>
 // 快速计算露点，速度是5倍的DHTx1_DewPoint()
 // 参考: http://en.wikipedia.org/wiki/Dew_point
 float DHTx1_DewPointFast(uint16_t ui16Celsius, uint16_t ui16Humidity)
@@ -244,11 +279,12 @@ float DHTx1_DewPointFast(uint16_t ui16Celsius, uint16_t ui16Humidity)
     float f32Celsius = (float)ui16Celsius / 10.0;
     float f32humidity = (float)ui16Humidity / 10.0;
 
-    float f32Tmp = (17.271 * f32Celsius) / (237.7 + f32Celsius) + log(f32humidity / 100);
-    
+    float f32Tmp = (17.271 * f32Celsius) / (237.7 + f32Celsius) + logf(f32humidity / 1000);
+
     return (237.7 * f32Tmp) / (17.271 - f32Tmp);
 }
 /*-----------------------------------------------------------------------------------------------*/
+#endif
 
 /*************************************************************************************************
 ************* Copyright 2020 (C), Sichuan Deyang DeYi Electronic Instrument Co., Ltd *************
--- src/Lib.DHTx1.h
+++ src/Lib.DHTx1.h
@@ -1,25 +1,27 @@
 /**
  * \file Lib.DHTx1.h
  * \author vEmagic (admin@vemgic.com)
- * \brief 
+ * \brief
  * \version 0.1.0
  * \date 2020-10-29
- * 
+ *
  * Copyright 2020 (C), Sichuan Deyang DeYi Electronic Instrument Co., Ltd.
- * 
+ *
  */
 
 #ifndef __LIB_DHTx1_H
 #define __LIB_DHTx1_H
 
 // #include "main.h"
+
+#if defined STM32
 #include "stm32f0xx_hal.h"
 
 typedef GPIO_TypeDef tPort;
 typedef uint32_t tPin;
 
 #define DHTx1_DelayUs(us)
-#define DHTx1_Millis()
+#define DHTx1_Millis() 0
 
 #define DHTx1_DATA_PIN_OUTPUT_MODE(port, pin) \
     LL_GPIO_SetPinMode((port), (pin), LL_GPIO_MODE_OUTPUT)
@@ -31,9 +33,39 @@ typedef uint32_t tPin;
 #define DHTx1_DATA_PIN_LOW(port, pin) LL_GPIO_ResetOutputPin((port), (pin))
 
 #define DHTx1_DATA_PIN_READ(port, pin) LL_GPIO_IsInputPinSet((port), (pin))
+
+#elif defined (SDCC) || defined (__SDCC)
+#include <stc12.h>
+#include "systimer.h"
+
+#define DHTx1_DATA_PIN P1_7
+
+typedef uint8_t tPort;
+typedef uint8_t tPin;
+
+#define DHTx1_DelayUs(us) \
+do {                      \
+    Timer0Restart(us);    \
+    while (false == Timer0IsElapsed()); \
+} while (0)
+
+#define DHTx1_Millis() 0
+
+#define DHTx1_DATA_PIN_OUTPUT_MODE(port, pin)
+#define DHTx1_DATA_PIN_INPUT_MODE(port, pin)
+
+#define DHTx1_DATA_PIN_HIGH(port, pin) (DHTx1_DATA_PIN = 1)
+#define DHTx1_DATA_PIN_LOW(port, pin) (DHTx1_DATA_PIN = 0)
+
+#define DHTx1_DATA_PIN_READ(port, pin) (DHTx1_DATA_PIN)
+
+#else
+#error "Unkown MCU platform!"
+#endif
 /*-----------------------------------------------------------------------------------------------*/
 
 typedef enum {
+    eDHTx1_INVAILD = -1,
     eDHTx1_OPENED = 0,
     eDHTx1_CLOSED,
     eDHTx1_DATA_CHECK_SUM_OK,
@@ -63,22 +95,20 @@ typedef struct {
 
 /**
  * \fn int8_t DHTx1_Init(tDHTx1Sensor* psDHTx1)
- * 
+ *
  * \brief Initial DHTx1 Device
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return int8_t 0 - Success, or Failed
  */
-
-
 int8_t DHTx1_Init(tDHTx1Sensor* psDHTx1);
 /*-----------------------------------------------------------------------------------------------*/
 
 /**
  * \fn tDHTx1Status DHTx1_Open(tDHTx1Sensor* psDHTx1)
- * 
+ *
  * \brief Open the DHTx1 Device
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return tDHTx1Status If open success, return the enum 'eDHTx1_OPENED';
  *                      or if DHTx1 response low level error, return 'eDHTx1_OPEN_RSP_LOW_ERROR',
@@ -90,7 +120,7 @@ tDHTx1Status DHTx1_Open(tDHTx1Sensor* psDHTx1);
 /**
  * \fn bool DHTx1_Close(tDHTx1Sensor* psDHTx1)
  * \brief Colse the DHTx1 Device
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return true Close Sucess, or false
  */
@@ -100,7 +130,7 @@ bool DHTx1_Close(tDHTx1Sensor* psDHTx1);
 /**
  * \fn uint8_t DHTx1_ReadByte(tDHTx1Sensor* psDHTx1)
  * \brief Read a byte data from the DHTx1 Device
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return uint8_t The DHTx1 device data
  */
@@ -110,17 +140,17 @@ uint8_t DHTx1_ReadByte(tDHTx1Sensor* psDHTx1);
 /**
  * \fn bool DHTx1_ReadData(tDHTx1Sensor* psDHTx1)
  * \brief Read the DHTx1 Device Data
- * 
+ *
  * \param psDHTx1 Pointer the \ref tDHTx1Sensor instance
- * \return true 
- * \return false 
+ * \return true
+ * \return false
  */
 bool DHTx1_ReadData(tDHTx1Sensor* psDHTx1);
 
 /**
  * \fn bool DHTx1_IsShortOfReadTime(void)
- * \brief Check that the interval between reading data is too short 
- * 
+ * \brief Check that the interval between reading data is too short
+ *
  * \return true Interval between reading data is too short, or false
  */
 bool DHTx1_IsShortOfReadTime(tDHTx1Sensor* psDHTx1, uint32_t ui32Intervals);
@@ -129,17 +159,18 @@ bool DHTx1_IsShortOfReadTime(tDHTx1Sensor* psDHTx1, uint32_t ui32Intervals);
 /**
  * \fn bool DHTx1_IsDataChanged(tDHTx1Sensor* psDHTx1)
  * \brief Check the data whether has been changed
- * 
+ *
  * \param psDHTx1 Pointer the \ref tDHTx1Sensor instance
+ * \param pui8PreviousData Pointer the previous data buffer
  * \return true The data has been changed, or false
  */
-bool DHTx1_IsDataChanged(tDHTx1Sensor* psDHTx1);
+bool DHTx1_IsDataChanged(tDHTx1Sensor* psDHTx1, uint8_t* pui8PreviousData);
 /*-----------------------------------------------------------------------------------------------*/
 
 /**
  * \fn tTemperature DHTx1_GetTemperature(tDHTx1Sensor* psDHTx1)
  * \brief Get the temperature value
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return uin16_t Temperature value scale 10 time
  */
@@ -149,7 +180,7 @@ uint16_t DHTx1_GetTemperature(tDHTx1Sensor* psDHTx1);
 /**
  * \fn tHumidity DHTx1_GetHumidity(tDHTx1Sensor* psDHTx1)
  * \brief Get the humidity value
- * 
+ *
  * \param psDHTx1 Pointer to the \ref tDHTx1Sensor instance
  * \return uint16_t Humidity value scale 10 time
  */
@@ -157,9 +188,20 @@ uint16_t DHTx1_GetHumidity(tDHTx1Sensor* psDHTx1);
 /*-----------------------------------------------------------------------------------------------*/
 
 /**
+ * \fn DHTx1_GetHumidityAndTemperature(tDHTx1Sensor* psDHTx1, uint16_t* pui16Humidity, uint16_t* pui16Temperature)
+ * \brief
+ *
+ * \param psDHTx1
+ * \param pui16Humidity
+ * \param pui16Temperature
+ */
+void DHTx1_GetHumidityAndTemperature(tDHTx1Sensor* psDHTx1,
+    uint16_t* pui16Humidity, uint16_t* pui16Temperature);
+
+/**
  * \fn float DHTx1_ConvertC2F(uint16_t ui16Celsius)
  * \brief Convert the temperature from celsius to fahrenheit
- * 
+ *
  * \param ui16Celsius The celsius temperature value, the value scale 10 time
  * \return float Fahrenheit temperature
  */
@@ -169,9 +211,9 @@ float DHTx1_ConvertC2F(uint16_t ui16Celsius);
 /**
  * \fn float DHTx1_ConvertC2K(uint16_t ui16Celsius)
  * \brief Convert the temperature from celsius to kai
- * 
+ *
  * \param ui16Celsius The celsius temperature value, the value scale 10 time
- * \return float Kai temperature 
+ * \return float Kai temperature
  */
 float DHTx1_ConvertC2K(uint16_t ui16Celsius);
 /*-----------------------------------------------------------------------------------------------*/
@@ -179,7 +221,7 @@ float DHTx1_ConvertC2K(uint16_t ui16Celsius);
 /**
  * \fn float DHTx1_DewPoint(uint16_t ui16Celsius, uint16_t ui16Humidity)
  * \brief Get dew pointer value
- * 
+ *
  * \param ui16Celsius The celsius temperature value, the value scale 10 time
  * \param ui16Humidity The humidity value, the value scale 10 time
  * \return float Dew Pointer value
@@ -190,10 +232,10 @@ float DHTx1_DewPoint(uint16_t ui16Celsius, uint16_t ui16Humidity);
 /**
  * \fn float DHTx1_DewPointFast(uint16_t ui16Celsius, uint16_t ui16Humidity)
  * \brief Get dew pointer value
- * 
+ *
  * \param ui16Celsius The celsius temperature value, the value scale 10 time
  * \param ui16Humidity The humidity value, the value scale 10 time
- * \return float Dew Pointer value 
+ * \return float Dew Pointer value
  */
 float DHTx1_DewPointFast(uint16_t ui16Celsius, uint16_t ui16Humidity);
 /*-----------------------------------------------------------------------------------------------*/
